7ec1aac2f03bd5c963c3f08a65796c1cda188064
BTLiao <76395159+BTLiao@users.noreply.github.com>
2021-01-26 19:31:17 +0800

    這是期末專案的母檔架構，請珍惜使用

---
+++ src/main/java/tw/leader/VintageApplication.java
@@ -0,0 +1,13 @@
+package tw.leader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VintageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VintageApplication.class, args);
+	}
+
+}

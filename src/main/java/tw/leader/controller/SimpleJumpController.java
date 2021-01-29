23c959aefd7c2c961e2530009e5e4a8abf561d2c
abi <xup6tony@gmail.com>
2021-01-29 09:27:03 +0800

    拿到不必要的impoort

--- src/main/java/tw/leader/controller/SimpleJumpController.java
+++ src/main/java/tw/leader/controller/SimpleJumpController.java
@@ -1,15 +1,7 @@
 package tw.leader.controller;
 
-import java.io.File;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class SimpleJumpController {
